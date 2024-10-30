22a8270b0e3aab524680408a758713336d534efc
Ethan <ethaneswanebban@gmail.com>
2024-10-30 23:06:17 +1100

    temp-create new file for temporary create tab screen which follows login need to fix some issue with image icon and handle create request and finishibng off design/schema needs to be fixed/controllers and routes created for event/

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,14 @@
-import { Router } from 'express';
-import userRoute from './user-route.js';
+import { Router } from "express";
+import userRoute from "./user-route.js";
+import eventRoute from "./event-route.js";
 
 const indexRoute = Router();
 
-indexRoute.get('', async (req, res) => {
-  res.json({ message: 'Welcome!' });
+indexRoute.get("", async (req, res) => {
+  res.json({ message: "Welcome!" });
 });
 
-indexRoute.use('/users', userRoute);
+indexRoute.use("/users", userRoute);
+indexRoute.use("/events", eventRoute);
 
 export default indexRoute;
