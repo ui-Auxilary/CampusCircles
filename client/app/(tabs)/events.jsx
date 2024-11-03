4d008efaf42f95803b0b1ce3104357ed2c5056b7
Christian Lam <christian.lam0103@gmail.com>
2024-11-03 17:30:10 +1100

    feat: Add basic map

--- client/app/(tabs)/events.jsx
+++ client/app/(tabs)/events.jsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
+import MapView from 'react-native-maps';
 
 const EventTab = () => {
   return (
-    <View>
-      <Text>Events</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 };
 
 export default EventTab;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
