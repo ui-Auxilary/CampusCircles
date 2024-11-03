2973e8e6553d8c5f73a0092f102d1e24ce71be10
Christian Lam <christian.lam0103@gmail.com>
2024-11-03 17:50:30 +1100

    feat: Basic map view of UNSW

--- client/app/(tabs)/events.jsx
+++ client/app/(tabs)/events.jsx
@@ -1,11 +1,26 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 const EventTab = () => {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        initialRegion={{
+          latitude: -33.91719,
+          longitude: 151.233033,
+          latitudeDelta: 0.0088,
+          longitudeDelta: 0.0091,
+        }}
+        style={styles.map}
+      >
+        <Marker
+          key={1}
+          coordinate={{ latitude: -33.91719, longitude: 151.233033 }}
+          title={'Cool marker'}
+          description={'Test marker'}
+        />
+      </MapView>
     </View>
   );
 };
