41b9effe42804e5f895868c40dfa19e8f917e822
ethanwong3 <ethaneswanebban@gmail.com>
2024-11-04 16:19:05 +1100

    homepage skeleton

--- client/app/(tabs)/index.jsx
+++ client/app/(tabs)/index.jsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import React from "react";
 
 const HomeTab = () => {
   return (
-    <View>
-      <Text>Homepage</Text>
+    <View style={styles.homepage}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}></Text>
+      </View>
+      <View style={styles.notifications}>
+        <Text style={styles.subtitleText}></Text>
+        <ScrollView></ScrollView>
+      </View>
+      <View style={styles.events}>
+        <Text style={styles.subtitleText}></Text>
+        <ScrollView></ScrollView>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  homepage: { backgroundColor: "#EEEEEE" },
+  titleContainer: { flex: 1 },
+  notifications: { flex: 5 },
+  events: { flex: 5 },
+  titleText: {},
+  subtitleText: {},
+});
 
 export default HomeTab;
