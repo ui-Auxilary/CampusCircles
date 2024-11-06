427c78700896a9066d5f4ae07b956a4769f637ff
ethanwong3 <ethaneswanebban@gmail.com>
2024-11-06 11:11:38 +1100

    hello

--- client/app/(tabs)/create.jsx
+++ client/app/(tabs)/create.jsx
@@ -1,7 +1,9 @@
 /*
 NOTES
 - LATER create button onPress needs to redirect user event detail page
-- NOW add permission for image picker
+- LATER add permission for image picker
+- NOW on event creation, retrieve event ID, connect it to the user
+- NOW do router.push({pathname: 'event-details', params: {id: [event-id]}})
 */
 
 import {
@@ -13,37 +15,40 @@ import {
   TextInput,
   Alert,
   ScrollView,
-} from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { router, useNavigation } from 'expo-router';
-import axios from 'axios';
-import * as ImagePicker from 'expo-image-picker';
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { router, useNavigation } from "expo-router";
+import axios from "axios";
+import * as ImagePicker from "expo-image-picker";
 
 // imported assets
-import pic from '../../assets/images/Image.png';
-import unchecked from '../../assets/images/unchecked.png';
-import checked from '../../assets/images/checked.png';
-import { BASE_URL } from '@/constants/api';
+import pic from "../../assets/images/Image.png";
+import unchecked from "../../assets/images/unchecked.png";
+import checked from "../../assets/images/checked.png";
+import { BASE_URL } from "@/constants/api";
 
 ///////////////////////////////////////////////////////////////////////////////
 // APP ////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
 const CreateTab = () => {
-  const [eventType, setEventType] = useState('Hang');
+  // default event type and image
+  const [eventType, setEventType] = useState("Hang");
+  const [image, setImage] = useState(null);
 
+  // default event obj
   const defaultEventData = {
-    name: '',
-    photo: '',
-    location: '',
+    name: "",
+    photo: "",
+    location: "",
     date: new Date(),
     time: new Date(),
-    description: '',
+    description: "",
     public: true,
     tag: {
       connectOrCreate: {
         where: {
-          id: '6722204d9e232f55d0a0903c',
+          id: "6722204d9e232f55d0a0903c",
         },
         create: {
           name: eventType,
@@ -55,22 +60,24 @@ const CreateTab = () => {
     invitations: {},
     creator: {
       connect: {
-        id: '6722204d9e232f55d0a0903c',
+        id: "6722204d9e232f55d0a0903c",
       },
     },
   };
 
+  // duplicate even object to return
   const [event, setEvent] = useState(defaultEventData);
-  const [image, setImage] = useState(null);
-  const navigation = useNavigation();
 
-  // Clear form on component mount or when navigated to
+  // clear form on component mount or when navigated to
+  const navigation = useNavigation();
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       resetForm();
     });
     return unsubscribe;
   }, [navigation]);
+
+  // functions: on form update
 
   const handleInputChange = (field, value) => {
     setEvent((prevEvent) => ({ ...prevEvent, [field]: value }));
@@ -78,11 +85,11 @@ const CreateTab = () => {
 
   const getTag = () => {
     switch (eventType) {
-      case 'Study':
+      case "Study":
         return styles.containerStudy;
-      case 'Eat':
+      case "Eat":
         return styles.containerEat;
-      case 'Other':
+      case "Other":
         return styles.containerOther;
       default:
         return styles.containerHang;
@@ -115,6 +122,8 @@ const CreateTab = () => {
     setEvent({ ...event, society: !event.society });
   };
 
+  // functions: on form submission
+
   const validateDate = (value) => {
     const datePattern = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
     return datePattern.test(value);
@@ -126,26 +135,26 @@ const CreateTab = () => {
   };
 
   const validateForm = () => {
-    const requiredFields = ['name', 'location', 'date', 'time', 'description'];
+    const requiredFields = ["name", "location", "date", "time", "description"];
     for (let field of requiredFields) {
       if (!event[field]) {
-        Alert.alert('Form Incomplete', `Please fill out the ${field} field.`);
+        Alert.alert("Form Incomplete", `Please fill out the ${field} field.`);
         return false;
       }
     }
 
     if (!validateDate(event.date)) {
       Alert.alert(
-        'Invalid Date Format',
-        'Please enter date in DD/MM/YYYY format.'
+        "Invalid Date Format",
+        "Please enter date in DD/MM/YYYY format."
       );
       return false;
     }
 
     if (!validateTime(event.time)) {
       Alert.alert(
-        'Invalid Time Format',
-        'Please enter time in 24-hour HH:MM format.'
+        "Invalid Time Format",
+        "Please enter time in 24-hour HH:MM format."
       );
       return false;
     }
@@ -158,7 +167,7 @@ const CreateTab = () => {
       return;
     }
 
-    console.log('POSTING', event);
+    console.log("POSTING", event);
     let postData = event;
     postData.date = new Date();
     postData.time = new Date();
@@ -166,11 +175,9 @@ const CreateTab = () => {
     axios
       .post(`${BASE_URL}/events/create`, event)
       .then(() => {
-        router.push('/create-event');
+        router.push("/event-details"); // change listed above
       })
       .catch((e) => console.log(e));
-
-    // resetForm();
   };
 
   const resetForm = () => {
@@ -189,14 +196,14 @@ const CreateTab = () => {
           <Pressable
             style={[
               styles.typeButtonFirst,
-              eventType === 'Hang' && styles.typeButtonFirstInverted,
+              eventType === "Hang" && styles.typeButtonFirstInverted,
             ]}
-            onPress={() => toggleTag('Hang')}
+            onPress={() => toggleTag("Hang")}
           >
             <Text
               style={[
                 styles.typeText,
-                eventType === 'Hang' && styles.typeTextInverted,
+                eventType === "Hang" && styles.typeTextInverted,
               ]}
             >
               Hang
@@ -205,14 +212,14 @@ const CreateTab = () => {
           <Pressable
             style={[
               styles.typeButton,
-              eventType === 'Study' && styles.typeButtonInverted,
+              eventType === "Study" && styles.typeButtonInverted,
             ]}
-            onPress={() => toggleTag('Study')}
+            onPress={() => toggleTag("Study")}
           >
             <Text
               style={[
                 styles.typeText,
-                eventType === 'Study' && styles.typeTextInverted,
+                eventType === "Study" && styles.typeTextInverted,
               ]}
             >
               Study
@@ -221,14 +228,14 @@ const CreateTab = () => {
           <Pressable
             style={[
               styles.typeButton,
-              eventType === 'Eat' && styles.typeButtonInverted,
+              eventType === "Eat" && styles.typeButtonInverted,
             ]}
-            onPress={() => toggleTag('Eat')}
+            onPress={() => toggleTag("Eat")}
           >
             <Text
               style={[
                 styles.typeText,
-                eventType === 'Eat' && styles.typeTextInverted,
+                eventType === "Eat" && styles.typeTextInverted,
               ]}
             >
               Eat
@@ -237,14 +244,14 @@ const CreateTab = () => {
           <Pressable
             style={[
               styles.typeButtonLast,
-              eventType === 'Other' && styles.typeButtonLastInverted,
+              eventType === "Other" && styles.typeButtonLastInverted,
             ]}
-            onPress={() => toggleTag('Other')}
+            onPress={() => toggleTag("Other")}
           >
             <Text
               style={[
                 styles.typeText,
-                eventType === 'Other' && styles.typeTextInverted,
+                eventType === "Other" && styles.typeTextInverted,
               ]}
             >
               Other
@@ -269,9 +276,9 @@ const CreateTab = () => {
               <Text style={styles.label}>Event Name</Text>
               <TextInput
                 style={[styles.field, { width: 200 }]}
-                placeholder='Enter event name'
+                placeholder="Enter event name"
                 value={event.name}
-                onChangeText={(value) => handleInputChange('name', value)}
+                onChangeText={(value) => handleInputChange("name", value)}
               />
             </View>
             {/* Location */}
@@ -279,9 +286,9 @@ const CreateTab = () => {
               <Text style={styles.label}>Location</Text>
               <TextInput
                 style={[styles.field, { width: 200 }]}
-                placeholder='Enter event location'
+                placeholder="Enter event location"
                 value={event.location}
-                onChangeText={(value) => handleInputChange('location', value)}
+                onChangeText={(value) => handleInputChange("location", value)}
               />
             </View>
           </View>
@@ -293,9 +300,9 @@ const CreateTab = () => {
             <Text style={styles.label}>Date</Text>
             <TextInput
               style={[styles.field, { width: 180 }]}
-              placeholder='DD/MM/YYYY'
+              placeholder="DD/MM/YYYY"
               value={event.date}
-              onChangeText={(value) => handleInputChange('date', value)}
+              onChangeText={(value) => handleInputChange("date", value)}
             />
           </View>
           {/* Time */}
@@ -303,9 +310,9 @@ const CreateTab = () => {
             <Text style={styles.label}>Time</Text>
             <TextInput
               style={[styles.field, { width: 180 }]}
-              placeholder='HH:MM'
+              placeholder="HH:MM"
               value={event.time}
-              onChangeText={(value) => handleInputChange('time', value)}
+              onChangeText={(value) => handleInputChange("time", value)}
             />
           </View>
         </View>
@@ -315,9 +322,9 @@ const CreateTab = () => {
           <Text style={styles.label}>Description</Text>
           <TextInput
             style={styles.descriptionContainer}
-            placeholder='Describe the event details'
+            placeholder="Describe the event details"
             value={event.description}
-            onChangeText={(value) => handleInputChange('description', value)}
+            onChangeText={(value) => handleInputChange("description", value)}
             multiline
           />
         </View>
@@ -388,121 +395,121 @@ const styles = StyleSheet.create({
   // Background
   containerHang: {
     flex: 1,
-    backgroundColor: '#E7948D',
-    alignItems: 'center',
+    backgroundColor: "#E7948D",
+    alignItems: "center",
     rowGap: 5,
   },
   containerStudy: {
     flex: 1,
-    backgroundColor: '#A0B7EF',
-    alignItems: 'center',
+    backgroundColor: "#A0B7EF",
+    alignItems: "center",
     rowGap: 5,
   },
   containerEat: {
     flex: 1,
-    backgroundColor: '#F0D074',
-    alignItems: 'center',
+    backgroundColor: "#F0D074",
+    alignItems: "center",
     rowGap: 5,
   },
   containerOther: {
     flex: 1,
-    backgroundColor: '#EEEEEE',
-    alignItems: 'center',
+    backgroundColor: "#EEEEEE",
+    alignItems: "center",
     rowGap: 5,
   },
   // Type Selector
   typeContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     borderRadius: 10,
     marginVertical: 25,
   },
   typeButtonFirst: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
   typeButton: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
   typeButtonLast: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
   typeButtonFirstInverted: {
-    backgroundColor: '#3A72FF',
+    backgroundColor: "#3A72FF",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
   },
   typeButtonInverted: {
-    backgroundColor: '#3A72FF',
+    backgroundColor: "#3A72FF",
     paddingHorizontal: 20,
     paddingVertical: 10,
   },
   typeButtonLastInverted: {
-    backgroundColor: '#3A72FF',
+    backgroundColor: "#3A72FF",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
   },
   typeText: {
-    color: '#454545',
+    color: "#454545",
     fontSize: 25,
   },
   typeTextInverted: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 25,
   },
   // Privacy Selector
   privacyContainer: {
     marginTop: 15,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
   },
   privacyButtonLeft: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
     paddingHorizontal: 65,
     paddingVertical: 10,
   },
   privacyButtonRight: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
     paddingHorizontal: 65,
     paddingVertical: 10,
   },
   privacyButtonLeftInverted: {
-    backgroundColor: '#3A72FF',
+    backgroundColor: "#3A72FF",
     paddingHorizontal: 65,
     paddingVertical: 10,
     borderTopLeftRadius: 10,
     borderBottomLeftRadius: 10,
   },
   privacyButtonRightInverted: {
-    backgroundColor: '#3A72FF',
+    backgroundColor: "#3A72FF",
     paddingHorizontal: 65,
     paddingVertical: 10,
     borderTopRightRadius: 10,
     borderBottomRightRadius: 10,
   },
   privacyText: {
-    color: '#454545',
+    color: "#454545",
     fontSize: 20,
   },
   privacyTextInverted: {
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 20,
   },
   // Event Details
@@ -510,36 +517,36 @@ const styles = StyleSheet.create({
     rowGap: 5,
   },
   label: {
-    color: '#454545',
+    color: "#454545",
     fontSize: 20,
   },
   field: {
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 10,
     height: 45,
     padding: 5,
   },
   imageContainer: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     height: 160,
     width: 160,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 25,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   fullImage: {
-    height: '100%',
-    width: '100%',
-    resizeMode: 'cover',
+    height: "100%",
+    width: "100%",
+    resizeMode: "cover",
   },
   iconImage: {
     height: 30,
     width: 30,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   descriptionContainer: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     height: 160,
     width: 375,
     borderRadius: 10,
@@ -547,33 +554,33 @@ const styles = StyleSheet.create({
   },
   // Society
   societyCreateContainer: {
-    width: '80%',
+    width: "80%",
     marginTop: 25,
   },
   // Create
   createButton: {
-    backgroundColor: '#76DA69',
+    backgroundColor: "#76DA69",
     paddingHorizontal: 27.5,
     paddingVertical: 12.5,
     borderRadius: 15,
     marginLeft: 100, // hardcoded
   },
   createText: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 25,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   // Other
   horiz: {
     columnGap: 15,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   verti: {
     rowGap: 5,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   shadow: {
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: { width: 3, height: 3 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
