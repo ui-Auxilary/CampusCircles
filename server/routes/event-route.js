ecdc4012f2770bc01df761964d0aaea0c0382980
Christian Lam <christian.lam0103@gmail.com>
2024-11-16 21:55:12 +1100

    feat: Add routes and update schema

--- server/routes/event-route.js
+++ server/routes/event-route.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { createEvent, getEvent, getEvents } from "../controllers/event.js";
+import {
+  createEvent,
+  getEvent,
+  getEvents,
+  getEventsToday,
+} from "../controllers/event.js";
 
 const eventRoute = Router();
 
 eventRoute.get("/get", getEvents);
+eventRoute.get("/get/today", getEventsToday);
 eventRoute.get("/get/:id", getEvent);
 eventRoute.post("/create", createEvent);
 
