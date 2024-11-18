9d138fa1c89a516076d531eb8aba75e6663a99b1
Sohee Lee <z5313630@ad.unsw.edu.au>
2024-11-18 22:08:27 +1100

    feat: joining and leaving event backend

--- server/routes/event-route.js
+++ server/routes/event-route.js
@@ -5,6 +5,8 @@ import {
   getEvents,
   getEventsToday,
   updateEvent,
+  joinEvent,
+  leaveEvent,
 } from "../controllers/event.js";
 
 const eventRoute = Router();
@@ -14,5 +16,7 @@ eventRoute.get("/get/today", getEventsToday);
 eventRoute.get("/get/:id", getEvent);
 eventRoute.post("/create", createEvent);
 eventRoute.put("/update/:id", updateEvent);
+eventRoute.post("/join", joinEvent);
+eventRoute.post("/leave", leaveEvent);
 
 export default eventRoute;
