dcc01aaa6304de7fd6739afed1e24539c7bdfcf0
Christian <chrisjinlam@gmail.com>
2024-11-20 16:21:33 +1100

    fix: fitx

--- client/app/event-edit.jsx
+++ client/app/event-edit.jsx
@@ -97,7 +97,7 @@ const EditEvent = () => {
 
     const fetchEvent = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/events/event/${id}`);
+        const response = await axios.get(`${BASE_URL}/events/get/${id}`);
         const fetchedEvent = response.data.data;
         console.log("Fetched event");
 
